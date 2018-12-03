f71f7b5fccb82ac4f4f79c31bf603593e19c80a1
Max Burton <2260452b@bo720-3-01u.dcs.gla.ac.uk>
2018-12-03 14:52:32 +0000

    basic database implementation

--- nutriplotterapp/App.js
+++ nutriplotterapp/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
+import { AppLoading, Asset, Font, Icon, SQLite } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+
+const db = SQLite.openDatabase('db.db');
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
   };
-
+  
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
       return (
