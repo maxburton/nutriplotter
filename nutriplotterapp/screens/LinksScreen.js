998aaf165dccc35bbafbfb3225014e62161456a9
Max Burton <2260452b@bo720-3-01u.dcs.gla.ac.uk>
2018-12-05 15:50:32 +0000

    Help Screen Fix

--- nutriplotterapp/screens/LinksScreen.js
+++ nutriplotterapp/screens/LinksScreen.js
@@ -1,27 +1,54 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import * as React from 'react';
+import { Constants, WebBrowser } from 'expo';
+import {Linking, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { MonoText } from '../components/StyledText';
 
-export default class LinksScreen extends React.Component {
+
+export default class HelpScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: "How to Play",
   };
 
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+  render(){
+    return(
+      <View >
+        <Text style = {styles.defaultText}>
+          The aim of the game is to build a balanced meal. The plus button allows you to look for foods to add and touching the plate allows you to edit amounts or remove foods from the plate.
+        </Text>
+
+        <Text style = {styles.defaultText}>
+          When you have finished your meal press the tick button to submit and see how you have done.
+        </Text>
+
+        <Text style = {styles.defaultText}>
+          If you are struggling read up on balanced diets here:
+        </Text>
+
+		    <TouchableOpacity onPress={this._handleOpenWithWebBrowser}>
+          <Text style={styles.buttonText}>Balanced Meal Reading</Text>
+        </TouchableOpacity>
+
+
+      </View>
     );
   }
+
+  _handleOpenWithWebBrowser = () => {
+    WebBrowser.openBrowserAsync('https://www.choosemyplate.gov/ten-tips-build-healthy-meal');
+  }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+  defaultText: {
+    color: 'black',
+    fontSize: 24,
+    padding: 15,
+    backgroundColor: "#00BFFF",
+	textAlign: 'center',
   },
+  buttonText: {
+	fontSize: 24,
+	textAlign: 'center',
+},
 });
